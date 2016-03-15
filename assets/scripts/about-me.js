d4735c1ace7dfa2593e78c32b75dfbc26dd262ae
mevans72 <michaleevans72@gmail.com>
2016-03-14 22:11:48 -0700

    Adding misc data to local storage to prove functionality. Adding etag based on temp json file to local storage to prove functionality.

--- assets/scripts/about-me.js
+++ assets/scripts/about-me.js
@@ -1,37 +1,55 @@
 var Mike = {};
 Mike.all = [];
-
-Mike.loadAll = function(rawData) {
-//   rawData.sort(function(a,b) {
-//     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-//   });
-
-  rawData.forEach(function(ele) {
-    Mike.all.push(new Mike(ele));
+//Put misc stuff in localStorage
+Mike.loadAll = function() {
+  Mike.all = [];
+  experienceData.forEach(function(el) {
+    Mike.all.push((el));
   });
-};
+  goalsData.forEach(function(el) {
+    Mike.all.push((el));
+  });
+  projectsData.forEach(function(el) {
+    Mike.all.push((el));
+  });
 
+};
+//Check for and/or set both localStorage and/or eTag
 Mike.fetchAll = function() {
+  var rawData;
   var storedEtag;
+
   $.ajax({
     type: 'HEAD',
-    url: 'portfolio/assets/scripts/about-me-data.js',
+    url: 'assets/scripts/about-me-project-data.json',
     success: function(data, message, xhr) {
       storedEtag = xhr.getResponseHeader('etag');
-      localStorage.setItem('etag', storedEtag);
-      if (storedEtag === localStorage.etag && localStorage.rawData) {
-        Mike.loadAll(localStorage.rawData);
-        // articleView.initIndexPage();
-      } else {
-        $.getJSON('portfolio/assets/scripts/about-me-data.js', function(rawData) {
-          console.log(rawData);
-          Mike.loadAll(rawData);
-          localStorage.setItem('rawData', JSON.stringify(rawData));
-          // articleView.initIndexPage();
-        });
-      }
     }
   });
-
+  if (localStorage.etag && localStorage.rawData) {
+    console.log('Both keys already exist in local storage');
+  // } else if (storedEtag !== localStorage.etag && localStorage.rawData) {
+  //   localStorage.setItem('etag', storedEtag);
+  //   console.log('Had to set etag in local storage...');
+  // } else if (storedEtag === localStorage.etag && !localStorage.rawData) {
+  //   Mike.loadAll();
+  //   var stringifiedMikeAll = JSON.stringify(Mike.all);
+  //   console.log('Had to set rawData in local storage...');
+  //   localStorage.setItem('rawData', stringifiedMikeAll);
+  } else {
+    Mike.loadAll();
+    var stringifiedMikeAll = JSON.stringify(Mike.all);
+    console.log(stringifiedMikeAll);
+    localStorage.setItem('rawData', stringifiedMikeAll);
+    $.ajax({
+      type: 'HEAD',
+      url: 'assets/scripts/about-me-project-data.json',
+      success: function(data, message, xhr) {
+        storedEtag = xhr.getResponseHeader('etag');
+        localStorage.setItem('etag', storedEtag);
+      }
+    });
+  };
 };
+//Run fetchAll
 Mike.fetchAll();
